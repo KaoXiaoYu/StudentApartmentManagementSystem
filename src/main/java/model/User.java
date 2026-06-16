ba726556212f7321f9320907002e8df3e6e38305
dust159679 <1298934004@qq.com>
2026-06-17 01:13:19 +0800

    更新 User 类的注释，增强代码可读性

---
+++ src/main/java/model/User.java
@@ -0,0 +1,19 @@
+package model;
+
+import com.jfinal.plugin.activerecord.Model;
+
+/**
+ * 用户实体类
+ * 继承 JFinal 的 Model 基类，采用 ActiveRecord 模式，用于操作用户相关的数据库表
+ *
+ * @param <User> 泛型参数，指定当前 Model 对应的实体类型
+ */
+public class User extends Model<User> {
+
+    /**
+     * 全局单例 DAO 对象
+     * 用于直接调用数据库增删改查操作，无需每次手动 new 实例
+     * 例如：User.dao.findById(id)
+     */
+    public static final User dao = new User().dao();
+}
