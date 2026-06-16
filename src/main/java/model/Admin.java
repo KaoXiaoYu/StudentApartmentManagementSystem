856b656d4c12e35db6b4039e699178a5cc191cdf
KaoXiaoYu <3575137428@qq.com>
2026-06-16 23:55:28 +0800

    ycb:submit 	deleted:    src/main/java/intercept/ApiExceptionInterceptor.java 	deleted:    src/main/java/intercept/AuthInterceptor.java 	deleted:    src/main/java/model/Admin.java 	deleted:    src/main/java/model/Appeal.java 	deleted:    src/main/java/model/College.java 	deleted:    src/main/java/model/RememberToken.java 	deleted:    src/main/java/model/StudentViolation.java 	deleted:    src/main/java/model/Teacher.java 	deleted:    src/main/java/model/User.java 	deleted:    src/main/java/service/PasswordService.java 	deleted:    src/main/java/service/UserService.java

--- src/main/java/model/Admin.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-/**
- * Compatibility model retained for old imports. Teacher accounts now use Teacher.
- */
-public class Admin extends Teacher {
-}
--- src/main/java/model/College.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-import com.jfinal.plugin.activerecord.Model;
-
-public class College extends Model<College> {
-    public static final College dao = new College().dao();
-}
